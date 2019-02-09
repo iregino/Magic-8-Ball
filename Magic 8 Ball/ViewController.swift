2eaa1fd29e5b566781ace592101bb7be05172f5c
student14 <student14@student14s-macbook-air.local>
2019-02-08 21:50:38 -0600

    Complete application

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Variables
+    var ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomBallNumber = 0
+    @IBOutlet weak var responseImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        newBallImage()
     }
-
-
+    // Invoke newBallImage when ask button is pressed
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        newBallImage()
+    }
+    // Randomly set a new ball image to imageView
+    func newBallImage() {
+        randomBallNumber = Int.random(in: 0...4)
+        responseImageView.image = UIImage(named: ballArray[randomBallNumber])
+    }
+    // Invoke newBallImage when device is shaken
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
 }
 
